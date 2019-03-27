f5da78b6f3c65b54e7449aa7113ad29b1c232de0
F4lcon <kevin@stieglitz-online.de>
2019-03-28 00:39:36 +0100

    Move Toolbar to Fragment

--- app/src/main/java/de/jambit/waller/ui/WallpaperList.kt
+++ app/src/main/java/de/jambit/waller/ui/WallpaperList.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.GridLayoutManager
 import de.jambit.waller.R
 import de.jambit.waller.adapter.WallpaperAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.wallpaper_list_fragment.*
 
 
@@ -27,6 +32,21 @@ class WallpaperList : Fragment() {
         return inflater.inflate(R.layout.wallpaper_list_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = this.activity
+        if (activity is AppCompatActivity) {
+            val navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+            setupBottomNavMenu(navController)
+            activity.setSupportActionBar(toolbar)
+//            NavigationUI.setupActionBarWithNavController(
+//                activity,
+//                navController
+//            )
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(requireActivity()).get(WallpaperListViewModel::class.java)
@@ -41,6 +61,13 @@ class WallpaperList : Fragment() {
 
         recyclerview_images.adapter = adapter
         recyclerview_images.layoutManager = GridLayoutManager(context, 2)
+    }
+
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
     }
 
 }
